5ad4e56df5578cc2109b0455196d1b9696a72ea0
Noelson <noelsonnunes@gmail.com>
2024-09-06 16:39:08 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,58 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    },
+    {   
+        titulo: "Leon Marchand",
+        descricao: "Nadador francês que conquistou múltiplas medalhas de ouro em Tóquio 2020 e é um dos grandes favoritos para Paris 2024.",
+        link: "https://en.wikipedia.org/wiki/Leon_Marchand",
+        tags: "natação, frança, ouro, recorde_mundial"
+      },
+      {
+        titulo: "Yulimar Rojas",
+        descricao: "Atleta venezuelana que dominou o salto triplo nos últimos anos, com recordes mundiais e olímpicos.",
+        link: "https://en.wikipedia.org/wiki/Yulimar_Rojas",
+        tags: "atletismo, salto_triplo, venezuela, recorde_mundial"
+      },
+      {
+        titulo: "Katie Ledecky",
+        descricao: "Nadadora americana, recordista mundial em diversas provas de meio fundo e fundo.",
+        link: "https://en.wikipedia.org/wiki/Katie_Ledecky",
+        tags: "natação, eua, recorde_mundial, meio_fundo"
+      },
+      {
+        titulo: "Rafaela Silva",
+        descricao: "Judoca brasileira, campeã olímpica no Rio 2016.",
+        link: "https://pt.wikipedia.org/wiki/Rafaela_Silva",
+        tags: "judô, brasil, ouro"
+      },
+      {
+        titulo: "Thiago Pereira",
+        descricao: "Nadador brasileiro, conhecido como Mr. Medley, com diversas medalhas em Campeonatos Mundiais e Jogos Pan-Americanos.",
+        link: "https://pt.wikipedia.org/wiki/Thiago_Pereira",
+        tags: "natação, brasil, medley"
+      },
+      {
+        titulo: "Martine Grael e Kahena Kunze",
+        descricao: "Dupla brasileira de vela, campeãs olímpicas na classe 49er FX em Tóquio 2020.",
+        link: "https://pt.wikipedia.org/wiki/Martine_Grael", // Link para uma delas, o outro pode ser adicionado como uma tag ou em uma propriedade separada
+        tags: "vela, brasil, ouro, dupla"
+      }
+         
+          
+];
